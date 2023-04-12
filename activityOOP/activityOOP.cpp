7d733bae72fdab45b68950d827010f2840eae7aa
Zharfan0 <zharfanwidyadana@gmail.com>
2023-04-12 10:49:20 +0700

    class mahasiswa public

--- activityOOP/activityOOP.cpp
+++ activityOOP/activityOOP.cpp
@@ -6,4 +6,12 @@ private:
 	int nim;
 	string nama;
 	float nilai;
-}
+public:
+	void input() {
+		cout << "masukan NIM: ";
+		cin >> nim;
+		cout << "masukan nama: ";
+		cin >> nama;
+		cout << "masukan nilai: ";
+		cin >> nilai;
+	}
